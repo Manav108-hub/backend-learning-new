37089dbc557661b4ce980def75235824f2098e63
manav108-hub <manavadwani86@gmail.com>
2025-10-18 20:46:05 +0000

    made user and video models with jwt

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import mongoose from "mongoose";
-import express from "express"
-import dotenv from "dotenv"
+import { app } from "./app.js";
+import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
 
 dotenv.config({
-    path : "./.env"
-})
-const app = express()
-const port = process.env.PORT || 3000
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
 
 connectDB()
-.then(() => {
-    app.listen(port , () => {
-        console.log(`Server is live on port ${port}`);
-    })
-    
-})
-.catch((err) => {
-    console.log("MONGO FAILED TO CONNECT" , err);
-    
-})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is live on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO FAILED TO CONNECT", err);
+  });
---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unqiue: true,
+      lowercase: true,
+      index: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unqiue: true,
+      lowercase: true,
+      index: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: true,
+      required: true,
+    },
+    coverImage: {
+      type: true,
+    },
+    watchHistory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { Timestamps: true }
+);
+
+// now using pre middleware so the passowrd first goes to the middleware for encrypting and then saving
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10); // 10 -> no. of rounds
+  next();
+});
+
+// now injecting a method for checking the password
+
+userSchema.methods.isPassword = async function (password) {
+    return await bcrypt.compare(password , this.password)
+}
+
+//generating access tokens and refresh tokens , why two because refresh tokens is stored in the db whereas access token is not 
+
+// sign -> payload , token , expire
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+        _id : this._id,
+        email : this.email,
+        fullname : this.fullname,
+        username : this.username
+    },
+    
+    process.env.JWT_ACCESS_TOKEN,
+    {
+        expiresIn : process.env.JWT_ACCESS_EXPIRY
+    }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id : this._id
+    },
+    process.env.JWT_REFRESH_TOKEN,
+    {
+        expiresIn: process.env.JWT_REFRESH_EXPIRY
+    }
+)
+}
+
+export default User = mongoose.Model("User", userSchema);
